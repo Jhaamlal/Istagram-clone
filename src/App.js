db7890b1eec7ce5ca9d5fc1da887610d4c86f545
ravi shankar <shankarji1729@gmail.com>
2021-05-31 21:31:31 +0530

    add pos.js and css changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import Post from'./Post'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      istagram adeded dd n ew code
-      </header>
+      
+        <div className="app_header">
+          <div className="app_headerImage">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+            height='50em' 
+            width='150em'
+            alt="Insta Logo"   />
+          </div>
+        </div>
+
+      <Post />
+     
     </div>
   );
 }
