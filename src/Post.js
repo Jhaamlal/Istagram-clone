c5f1ec3a5f20ab3ccdfcc2b2a29fe779d3f0f10d
ravi shankar <shankarji1729@gmail.com>
2021-05-31 22:30:36 +0530

    Add avatar in Icon

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import './Post.css'
+import { Button } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
 
 function Post() {
     return (
-        <div>
-            <h3>Username</h3>
+        <div className="post">
+            <div className="post_header">
+                <Avatar 
+                className='post_avatar'
+                alt="Semy Sharp" 
+                
+                src="/static/images/avatar/1.jpg" />
+                <h3>Username</h3>
+        </div>
+
+
+            <img className="post_image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt="" srcset="" />
             {/*  */}
+            <h4 className='post_text'> <strong>Username</strong> :caption</h4>
         </div>
     )
 }
